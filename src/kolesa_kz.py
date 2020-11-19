452a57534509f5a1a51aa1031ab473efe459c0f4
Argyn Khuan <argynkhuan@yandex.kz>
2020-11-19 17:18:14 -0600

    csv refactoring

--- src/kolesa_kz.py
+++ src/kolesa_kz.py
@@ -27,6 +27,15 @@ def get_total_pages(html):
 
     # return int(pages)
 
+def append_list_as_row(file_name, list_of_elem):
+    # Open file in append mode
+    with open(file_name, 'a+', newline='') as write_obj:
+        # Create a writer object from csv module
+        csv_writer = csv.writer(write_obj)
+        # Add contents of list as last row in the csv file
+        csv_writer.writerow(list_of_elem)
+
+
 def get_page_data(html):
     try:
         soup = BeautifulSoup(html, 'lxml')
